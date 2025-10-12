893b51fe2a219f2f73ad64d8fa218cad9b5350d6
Markram-WS <mark_AI01@hotmail.com>
2025-10-12 15:43:06 +0700

    [feature] binance get account

--- src/api/binance/account.rs
+++ src/api/binance/account.rs
@@ -1,12 +1,9 @@
-use axum::http::HeaderValue;
 use reqwest::Client;
 use std::error::Error;
-use chrono::Utc;
 use std::time::{SystemTime, UNIX_EPOCH};
+use crate::utils::{get_env,create_signature};
 
-use lib::utils::{get_env_decode,get_env,create_signature};
-
-pub async fn account_info() -> Result<(), Box<dyn Error>> {
+pub async fn account_info() -> Result<reqwest::Response , Box<dyn Error>> {
     let api_host = get_env("API_HOST");
     let api_secret = get_env("API_SECRET");
     let api_key = get_env("API_KEY");
@@ -18,47 +15,60 @@ pub async fn account_info() -> Result<(), Box<dyn Error>> {
     .to_string();
 
     let params: Vec<(&str, String)> = vec![
-            ("omitZeroBalances", "false".to_string()),
-            ("recvWindow", "60000".to_string()),
-            ("timestamp", timestamp.copy())
+            ("timestamp", timestamp)
     ];
 
-    let query_string = serde_urlencoded::to_string(&params).expect("urlencode failed");
-    let url = format!("https://{}/api/v3/account?{}&signature={}", api_host,query_string, signature);
+    let query_string = serde_urlencoded::to_string(&params)?;
+    let signature: String = create_signature(&params,&api_secret)?;
+    let url = format!("{}/api/v3/account?{}&signature={}", api_host,query_string, signature);
 
     let client = Client::new();
+
+    //println!("{}",&url);
     let res = client
         .get(&url)
-        .header("X-MBX-APIKEY", "xxxxx") // ใส่ API key
+        .header("X-MBX-APIKEY", &api_key) 
         .header("Accept", "application/json")
         .send()
         .await?;
-
-    // ตรวจสอบ status
-    if res.status().is_success() {
-        let body = res.text().await?;
-        println!("Response: {}", body);
-    } else {
-        eprintln!("Error: HTTP {}", res.status());
-    }
-
-    Ok(())
+    Ok(res)
 }
 
+#[cfg(test)]
 mod tests {
     use super::*;
     use std::env;
+    use std::sync::Once;
+    use dotenvy::dotenv;
+    static INIT: Once = Once::new();
+
+    
+    fn init() {
+        INIT.call_once(|| {
+            dotenv().ok();
+        });
+    }
+
     //get_env
     #[tokio::test]
-    async  fn test_api_account_info(){
-        let api_secret_test = get_env_decode("API_SECRET_TEST");
-        let api_key_test = get_env_decode("API_KEY_TEST");
+    async  fn test_api_binance_account_info(){
+        init();
+        let api_key = get_env("API_KEY_TEST");
+        let api_secret_test = get_env("API_SECRET_TEST");
         unsafe { 
-            env::set_var("API_HOST", "https://testnet.binance.vision/api");
-            env::set_var("API_SECRET_TEST", api_secret_test);
-            env::set_var("API_KEY_TEST", api_key_test);
-        };
-        assert_eq!(1,1)
+            env::set_var("API_HOST", "https://testnet.binance.vision");
+            env::set_var("API_SECRET", api_secret_test);
+            env::set_var("API_KEY", api_key);
 
+        };
+        let res: reqwest::Response = account_info().await.expect("fn error");
+        let status = res.status();
+
+        if !status.is_success() {
+            let body = res.text().await.expect("Failed to read body");
+            println!("{}", body);
+        }
+
+        assert_eq!(status.as_u16(), 200);
     }
 }
--- src/utils/request.rs
+++ src/utils/request.rs
@@ -1,47 +1,31 @@
-use hmac::{Hmac, Mac};
-use sha2::Sha256;
-use hex;
-use serde_urlencoded;
-use std::env;
+use base64::{engine::general_purpose, Engine as _};
+use ed25519_dalek::SigningKey;
+use ed25519_dalek::Signer;
+use pkcs8::DecodePrivateKey; // <— ต้องเพิ่มบรรทัดนี้
+use urlencoding::encode;
 
-type HmacSha256 = Hmac<Sha256>;
-
-pub fn create_signature( params: &Vec<(&str, String)>,api_secret:&str) ->  Result<String, Box<dyn std::error::Error>> {
-    let query_string  = serde_urlencoded::to_string(&params).expect("url params encode failed");
-    let mut mac = HmacSha256::new_from_slice(api_secret.as_bytes())
-        .expect("HMAC can take key of any size");
-    mac.update(query_string.as_bytes());
-    let signature_bytes = mac.finalize().into_bytes();
-    let signature = hex::encode(signature_bytes);
-    println!("{}",signature);
-    Ok(signature)
-
+pub fn create_signature( payload: &Vec<(&str, String)>,private_key:&str) ->  Result<String, Box<dyn std::error::Error>> {
+    let query_string  = serde_urlencoded::to_string(&payload).expect("url params encode failed");
+    let key_bytes = general_purpose::STANDARD.decode(private_key)?;
+    let signing_key = SigningKey::from_pkcs8_der(&key_bytes)
+        .map_err(|e| format!("failed to parse pkcs8 ed25519 key: {}", e))?;
+    let signature = signing_key.sign(query_string.as_bytes());
+    let signature_b64 = general_purpose::STANDARD.encode(signature.to_bytes());
+    let signature_encoded = encode(&signature_b64).into_owned();
+    //println!("{}", signature_encoded);
+    Ok(signature_encoded)
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::env;
     #[test]
-    fn test_create_signature() {
-        //use std::time::{SystemTime, UNIX_EPOCH};
-        // let timestamp: String = SystemTime::now()
-        //     .duration_since(UNIX_EPOCH)
-        //     .expect("time went backwards")
-        //     .as_millis()
-        //     .to_string();
-        
+    fn test_utils_create_signature() {
         let params: Vec<(&str, String)> = vec![
-            ("symbol", "BTCUSDT".to_string()),
-            ("side", "BUY".to_string()),
-            ("type", "LIMIT".to_string()),
-            ("quantity", "1".to_string()),
-            ("price", "45000".to_string()),
-            ("timestamp", "1760197335465".to_string()),
+            ("timestamp", "1760244698897".to_string()),
         ];
-        let api_secret = "your_secret_key";
-        assert_eq!(create_signature( &params,&api_secret).unwrap(), "eba0ec0433b2dcf16176be3df2701d7001fd9a743673d1489e9fd992513a66dc");
+        let api_secret = "MC4CAQAwBQYDK2VwBCIEIHvC/UOrqjH8NrhgY4gJFkgyfB359eC9Mofmj/qFvuzB";
+        assert_eq!(create_signature( &params,&api_secret).unwrap(), "qhA4ffi029s6iwNqaO9ex5qqjH7fZYVyBngZldEGm1rbXeexgwK2BK1%2F8NdviRmyq9kB1vXKCBVF4pk2C7SPBA%3D%3D");
     }
 
 }
